f1335ddbfbaa596026b4e46d6d5fd9e2ae91770a
Daniel Obara <daniel.tsutomu@hotmail.com>
2019-03-01 17:56:44 -0300

    Done module one
    Exercises module one done!

---
+++ Modulo1.js
@@ -0,0 +1,93 @@
+//Exercicio 1:
+
+var endereco = {
+  rua: "Rua dos pinheiros",
+  numero: 1293,
+  bairro: "Centro",
+  cidade: "São Paulo",
+  uf: "SP"
+};
+
+const { rua, numero, bairro, cidade, uf } = endereco;
+console.log(
+  `O usuário mora em ${cidade} / ${uf}, no bairro ${bairro}, na rua "${rua}" com nº ${numero}.`
+);
+
+/*--------------------------------------------------------------------------------------------------*/
+
+//Exercicio 2:
+
+const pares = (x, y) => {
+  for (var i = x; i <= y; i++) {
+    if (i % 2 === 0) {
+      console.log(i);
+    }
+  }
+};
+pares(1, 20);
+
+/*--------------------------------------------------------------------------------------------------*/
+
+//Exercicio 3:
+
+const validaSkill = skills => {
+  if (skills.indexOf("Javascript") !== -1) {
+    return console.log("Tem a skill");
+  } else {
+    console.log("Não tem a skill");
+  }
+};
+
+var skills = ["Boostrap", "React", "CSS", "HTML"];
+validaSkill(skills);
+validaSkill([...skills, "Javascript"]);
+
+/*--------------------------------------------------------------------------------------------------*/
+
+//Exercício 4
+
+const calcExp = age => {
+  switch (true) {
+    case age >= 0 && age <= 1:
+      console.log(`Você tem ${age} anos de exp, seu nível é: Iniciante`);
+      break;
+    case age >= 1 && age <= 3:
+      console.log(`Você tem ${age} anos de exp, seu nível é: Intermediário`);
+      break;
+    case age >= 3 && age <= 6:
+      console.log(`Você tem ${age} anos de exp, seu nível é: Avançado`);
+      break;
+    default:
+      console.log(`Você tem ${age} anos de exp, seu nível é: Jedi Master `);
+      break;
+  }
+};
+
+calcExp(1);
+calcExp(3);
+calcExp(6);
+calcExp(10);
+
+/*--------------------------------------------------------------------------------------------------*/
+
+//Exercício 5
+
+var usuarios = [
+  {
+    nome: "Diego",
+    habilidades: ["Javascript", "ReactJS", "Redux"]
+  },
+  {
+    nome: "Gabriel",
+    habilidades: ["VueJS", "Ruby on Rails", "Elixir"]
+  }
+];
+
+const showMessage = usuarios => {
+  for (const user of usuarios) {
+    const { nome, habilidades } = user;
+    console.log(`O ${nome} possui as habilidades: ${habilidades.join(", ")}`);
+  }
+};
+
+showMessage(usuarios);
--- README.md
+++ README.md
@@ -1,1 +1,89 @@
-Javascript-Rocketseat
+# JS Rocketseat Challenge Excercises :book:
+
+## This project is just the final exercises of each Rocketseat Javascript course module
+
+### **Exercícios: Módulo 01**
+
+**1º exercício** :pencil:
+
+Crie uma função que dado o objeto a seguir:
+
+```
+var endereco = {
+		rua: "Rua dos pinheiros",
+		numero: 1293,
+		bairro: "Centro",
+		cidade: "São Paulo",
+		uf: "SP"
+};
+```
+
+Retorne o seguinte conteúdo:
+
+`O usuário mora em São Paulo / SP, no bairro Centro, na rua "Rua dos Pinheiros" com nº 1293.`
+
+**2º exercício** :pencil:
+
+Crie uma função que dado um intervalo (entre x e y) exiba todos número pares:
+
+```
+function pares(x, y) {
+// código aqui
+}
+pares(32, 321);
+```
+
+**3º exercício** :pencil:
+
+Escreva uma função que verifique se o vetor de habilidades passado possui a habilidade "Javascript"
+e retorna um booleano true/false caso exista ou não.
+
+```
+function temHabilidade(skills) {
+// código aqui
+}
+var skills = ["Javascript", "ReactJS", "React Native"];
+temHabilidade(skills); // true ou false
+```
+
+_Dica: para verificar se um vetor contém um valor, utilize o método indexOf._
+
+**4º exercício** :pencil:
+
+Escreva uma função que dado um total de anos de estudo retorna o quão experiente o usuário é:
+
+```
+function experiencia(anos) {
+// código aqui
+}
+var anosEstudo = 7;
+experiencia(anosEstudo);
+// De 0-1 ano: Iniciante
+// De 1-3 anos: Intermediário
+// De 3-6 anos: Avançado
+// De 7 acima: Jedi Master
+```
+
+**5º exercício** :pencil:
+
+Dado o seguinte vetor de objetos:
+
+```
+var usuarios = [
+{
+nome: "Diego",
+habilidades: ["Javascript", "ReactJS", "Redux"]
+},
+{
+nome: "Gabriel",
+habilidades: ["VueJS", "Ruby on Rails", "Elixir"]
+}
+];
+```
+
+Escreva uma função que produza o seguinte resultado:
+
+`O Diego possui as habilidades: Javascript, ReactJS, Redux O Gabriel possui as habilidades: VueJS, Ruby on Rails, Elixir`
+
+_Dica: Para percorrer um vetor você deve utilizar a sintaxe for...of e para unir valores de um array
+com um separador utilize o join._
