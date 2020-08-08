a676b47960d47e59945ca77dd65519e1ff908f9b
Daniel Ramos de Carvalho <danielramosbh74@gmail.com>
2020-08-08 11:30:44 -0300

    Update ExerciciosModulo1.js
    Considering that your repository is so organized and so it's going to be very useful to me and many other people, I'll try to solve these exercises too in another ways or at least writting the variables names in english to practice and also try to follow some best programming practices.

--- ExerciciosModulo1/ExerciciosModulo1.js
+++ ExerciciosModulo1/ExerciciosModulo1.js
@@ -11,6 +11,30 @@ console.log(`O usuário mora em ${cidade} / ${uf}, no bairro
 ${bairro}, na rua "${rua}" com nº ${numero}.`);
 
 /*--------------------------------------------------------------------------------------------------*/
+
+// Alternative code for Exercise 1:
+
+// Module 01 - Exercise 1
+
+var address = {
+  street: "Rua dos pinheiros",
+  number: 1293,
+  district: "Centro",
+  city: "São Paulo",
+  state: "SP"
+ };
+
+function address2 () {
+  var res = 'O usuário mora em ' + address.city + ' / ' + address.state + ', no bairro ' + address.district + ', na rua ' + address.street + " com número " + address.number; 
+  return res;
+}
+
+var res = address2();
+
+console.log (res);
+
+/*--------------------------------------------------------------------------------------------------*/
+
 //Exercicio 2:
 const pares = (x, y) => {
   for (var i = x; i <= y; i++) {
